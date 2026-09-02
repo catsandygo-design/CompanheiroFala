d8e14440f1e32df8b1f42d5d1195c6315921cd37
catsandygo-design <catsandy.go@gmail.com>
2026-09-02 20:07:50 -0300

    Bump Companheiro Fala to v0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "br.com.companheirofala"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.7.0"
+        versionCode = 8
+        versionName = "0.8.0"
     }
 
     buildTypes {
