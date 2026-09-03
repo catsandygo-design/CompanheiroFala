ce31d9f7124e1f21504d0db69c195b6751d52248
catsandygo-design <catsandy.go@gmail.com>
2026-09-02 21:02:18 -0300

    Bump contextual companion to v0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "br.com.companheirofala"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.10.0"
     }
 
     buildTypes {
